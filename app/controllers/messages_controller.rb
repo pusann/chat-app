db441d7990a3484113393ac9a44626ab0a060595
pusann <rainbow.7.7west2014@gmail.com>
2021-12-25 19:44:53 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
   def create
     @room = Room.find(params[:room_id])
@@ -9,7 +10,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
-      render :index
+      @messages = @room.messages.includes(:user)
     end
   end
 
