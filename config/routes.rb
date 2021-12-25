720f5b9516dddd5651f95d2c91a193eda5fa2493
pusann <rainbow.7.7west2014@gmail.com>
2021-12-25 19:12:51 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
-end
+  resources :rooms, only: [:new, :create] do
+  resources :messages, only: [:index, :create]
+  end
